15bde10197c9e7fa1d5ba92db742ef832f4b5393
Aswathy <aswathyretnakaran@yahoo.com>
2020-06-01 19:10:51 +0530

    Adding initial files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={(e) => {
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state.searchField)
+            );
+          }}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
